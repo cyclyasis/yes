eb28644deba22ef99f3e132c2aa5459da588bbd5
cyclyasis <73409674+cyclyasis@users.noreply.github.com>
2020-10-24 19:52:11 -0400

    Create uhuh.js

---
+++ uhuh.js
@@ -0,0 +1,101 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
+<button>
+<h2> The Most Epic Site! </h2>
+<p> What Is Epicsite.org? <p>
+<p> EpicSite.org is a site where you can be epic! </p>
+<p> here are some cool sites for you! </p>
+
+<h1> No.1:
+Minecraft</h1>
+<p><a href = "www.minecraft.net">Play Minecraft!</a>
+
+<p> What Is Minecraft?</p>
+
+<p> Minecraft Is A SinglePlayer And MultiPlayer Game, Where You Can Explore, Hunt, And Survive!</p>
+
+<h1> No.2: Discord</h1>
+
+<p><a href = "www.discord.com">Discord, go and stop using viber!</a>
+
+<p> I think you know all these sites, so get off this website! </p>
+<p> But before you do, please like this video! </p>
+<p><a href = “https://bit.ly/36k8dJh”>This Is A Cool Video</a>
+</button>
+<p> Also Here, Put Some Stuff In Here! We Currently Have No Database So This Login Is Pointless</p>
+
+<form method="post">
+Username
+<input type="text" name="fname"/>
+Password
+<input type="text" name="lname"/>
+<button onclick="alert('Whats Up')">Login</button>
+<h2> Or Sign Up</h2>
+
+
+<form method="post">
+Username
+<input type="text" name="fname"/>
+Password
+<input type="text" name="lname"/>
+<button onclick="alert('Welcome! Hope You Enjoy My Site!')">Sign Up</button>
+<h3> Thanks For Visiting Our Site! We Will Try To Make Slight Upgrades Every Month Or So On!</h3>
+
+<h3> Also, Do You Love Our Site?</h3>
+<button onclick="alert('Awww')">yes</button>
+<button onclick="alert('burn in hell')">No, I hate it.</button>
+
+<h2> Anyways, Here's Some Cool Stuff On The Site!</h2>
+     <h2> Do You Love Life?</h2>
+     <button onclick="alert('Really? Are You That Dumb?')">I Love Life</button> <button onclick="alert('You Are Very Smart')"> I Hate Life</button>
+     
+     
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
+<button onclick="myFunction()">Don't click me pls</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var txt;
+  if (confirm("wiping hardrive...")) {
+    txt = "You are so nice for letting us wipe your hardrive!";
+  } else {
+    txt = "You are sooo racist!!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
